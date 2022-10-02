a9605a0b53a5fd0be7177c9a0f5585942e5600ab
Hamza-ABDOULHOUSSEN <abd.hamza.ha@gmail.com>
2022-10-02 11:57:10 +0200

    AlertBloc function

--- server.R
+++ server.R
@@ -3,6 +3,22 @@ library(ggplot2)
 
 shinyServer ( function (input , output ) {
   
+  AlertBloc <- reactive({
+    if (input$n <= 0) {
+      return(HTML(paste(
+        '<div class="alert alert-warning alert-dismissible fade show" role="alert">',
+        '<strong>n <= 0 </strong> Attention, le nombre de tirages est inférieur ou égal à 0',
+        '<button type="button" class="close" data-dismiss="alert" aria-label="Close">',
+        '<span aria-hidden="true">&times;</span>',
+        '</button>',
+        '</div>',
+      )))
+    }
+    else {
+      return(HTML(''))
+    }
+  })
+  
   Simulate <- eventReactive(input$go, {
     HTML(paste(
     box(background = "black", width=12,
@@ -10,41 +26,15 @@ shinyServer ( function (input , output ) {
         
         fluidRow(
           infoBox("Mise de départ", width = 6, input$m, icon = icon("list"), color="blue", fill=TRUE),
-          infoBox("Nombre de tirages", width = 6, input$n, icon = icon("list"), color="blue", fill=TRUE)
+          infoBox("Nombre de tirages", width = 6, input$n, icon = icon("list"), color="blue", fill=TRUE),
+          AlertBloc()
         )
         
     )))
   })
   
-  # Simulate if n is null or negative
-  Simulate_nev <- eventReactive(input$go, {
-    HTML(paste(
-      box(background = "black", width=12,
-          HTML("<h2> Résultats :</h2>" ),
-          
-          fluidRow(
-            infoBox("Mise de départ", width = 6, input$m, icon = icon("list"), color="blue", fill=TRUE),
-            infoBox("Nombre de tirages", width = 6, input$n, icon = icon("list"), color="blue", fill=TRUE),
-            HTML(paste(
-              '<div class="alert alert-warning alert-dismissible fade show" role="alert">',
-              '<strong>n <= 0 </strong> Attention, le nombre de tirages est inférieur ou égal à 0',
-              '<button type="button" class="close" data-dismiss="alert" aria-label="Close">',
-              '<span aria-hidden="true">&times;</span>',
-              '</button>',
-              '</div>',
-            ))
-          )
-      ),
-
-      ))
-  })
-  
   output$result <- renderText({
-    if (input$n <= 0) {
-      Simulate_nev()
-    }
     Simulate()
-    
   })
 
 
